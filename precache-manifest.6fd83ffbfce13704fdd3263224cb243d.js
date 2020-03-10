c1ef7b40a67f9e225ace5bec96b4573e2579be60
Hieu <trunghieu99tt@gmail.com>
2020-03-10 18:09:34 +0700

    Updates

---
+++ precache-manifest.6fd83ffbfce13704fdd3263224cb243d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdc4087762f5e45c22e9a1a039f14a0",
+    "url": "/Video-Blog/index.html"
+  },
+  {
+    "revision": "7445fee921911bc2db26",
+    "url": "/Video-Blog/static/css/2.60325dfa.chunk.css"
+  },
+  {
+    "revision": "6d494928b1aefa981de5",
+    "url": "/Video-Blog/static/css/main.fb9fc439.chunk.css"
+  },
+  {
+    "revision": "7445fee921911bc2db26",
+    "url": "/Video-Blog/static/js/2.e67220ea.chunk.js"
+  },
+  {
+    "revision": "ce760cbb17a708da6049e8bc7bb2e776",
+    "url": "/Video-Blog/static/js/2.e67220ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d494928b1aefa981de5",
+    "url": "/Video-Blog/static/js/main.8e78dd3a.chunk.js"
+  },
+  {
+    "revision": "686e01ed078d3c2ff7c7",
+    "url": "/Video-Blog/static/js/runtime-main.c9efa2c9.js"
+  },
+  {
+    "revision": "37b5c5d914d1008fae09f6d6f20c953b",
+    "url": "/Video-Blog/static/media/bg-01.37b5c5d9.jpg"
+  }
+]);
